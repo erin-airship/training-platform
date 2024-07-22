b10b5af1f3e4346b250d10fc9f81652ace0a3572
Erin Airship <erin.isbell@unosquare.com>
2024-07-22 09:12:26 -0500

    add hello world route

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { Request, Response } from 'express';
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.get('/hello-world', (req: Request, res: Response) => {
+  res.json({ hello: 'World' });
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the home page!');
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
+
