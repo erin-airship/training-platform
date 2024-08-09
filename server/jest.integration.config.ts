10fc0c7c3fca34606328fccb8103f1060ad88482
Erin Airship <erin.isbell@unosquare.com>
2024-08-09 10:26:33 -0500

    Adding more integration tests

--- server/jest.integration.config.ts
+++ server/jest.integration.config.ts
@@ -2,7 +2,7 @@ const config = {
     globalSetup: "./test-setup/global-setup.js",
     globalTeardown: "./test-setup/global-teardown.js",
     clearMocks: true,
-    testTimeout: 15000,
+    testTimeout: 1500,
     testEnvironment: "node",
     preset: "ts-jest",
   };
