1196a2214019729b8132eafc7c6282a76bd9c003
Erin Airship <erin.isbell@unosquare.com>
2024-08-19 08:47:37 -0500

    Adding Vite, React Router, and setting up the client

---
+++ client/src/routes/login/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/shared/Navbar";
+
+const LogIn = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Please log in to access the platform</div>
+    </>
+  );
+};
+
+export default LogIn;
