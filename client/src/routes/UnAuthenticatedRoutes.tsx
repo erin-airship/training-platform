8a412e9b061b239f2930bd5cd7702fcf1cc8acf4
Erin Airship <erin.isbell@unosquare.com>
2024-08-21 11:21:24 -0500

    Add Signup Route

--- client/src/routes/UnAuthenticatedRoutes.tsx
+++ client/src/routes/UnAuthenticatedRoutes.tsx
@@ -1,10 +1,12 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
 import LogIn from './login';
+import SignUp from './signup';
 
 export const UnauthenticatedRoutes = () => {
     return (
       <Routes>
         <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     )
